cd41f0593599b38c86817450a88018016e132822
Mohit Soni <mohitsoni96705@gmail.com>
2026-05-03 22:20:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (66.88%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -0,0 +1,17 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        st = []
+        res = []
+
+        while root or st:
+            while root:
+                st.append(root)
+                root = root.left
+            
+            root = st.pop()
+            res.append(root.val)
+
+            root = root.right
+        
+        return res   
+        
